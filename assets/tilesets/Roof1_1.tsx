f2113679ecc731308e1e804aadf03f6e9184da20
CharlieUtting <charlieutting8@gmail.com>
2022-07-05 11:46:57 +0100

    Made new tilemap

---
+++ assets/tilesets/Roof1_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Roof1_1" tilewidth="32" tileheight="32" margin="2" tilecount="9" columns="3">
+ <image source="../../../../../Downloads/craftpix-551899-free-zombie-tds-tilesets-buildings-and-furniture/Tiles/PNG_version_2/Roofs/Roof1_1.png" width="128" height="128"/>
+</tileset>
