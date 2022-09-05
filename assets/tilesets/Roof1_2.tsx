e8e7499624435b0ea730d7797fbb478249867dca
CharlieUtting <charlieutting8@gmail.com>
2022-09-05 21:01:44 +0100

    collisions

--- assets/tilesets/Roof1_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="Roof1_2" tilewidth="32" tileheight="32" margin="2" tilecount="9" columns="3">
- <image source="../../../../../Downloads/craftpix-551899-free-zombie-tds-tilesets-buildings-and-furniture/Tiles/PNG_version_2/Roofs/Roof1_2.png" width="128" height="128"/>
-</tileset>
